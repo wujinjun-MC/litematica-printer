73444f8b3ab42284aaf4e844d35baf267204c0c9
wujinjun-MC <89054683+wujinjun-MC@users.noreply.github.com>
2026-07-21 00:28:17 +0800

    Extend printing range
    Increased the maximum value for PRINTING_RANGE from 5 to 64.

--- src/main/java/me/aleksilassila/litematica/printer/config/Configs.java
+++ src/main/java/me/aleksilassila/litematica/printer/config/Configs.java
@@ -14,7 +14,7 @@ public class Configs {
 
     // Configs settings
     public static final ConfigInteger PRINTING_INTERVAL = new ConfigInteger("printingInterval", 12, 1, 40).apply(GENERIC_KEY);
-    public static final ConfigDouble PRINTING_RANGE = new ConfigDouble("printingRange", 5, 2.5, 5).apply(GENERIC_KEY);
+    public static final ConfigDouble PRINTING_RANGE = new ConfigDouble("printingRange", 5, 2.5, 64).apply(GENERIC_KEY);
     public static final ConfigBoolean PRINT_MODE = new ConfigBoolean("printingMode", false).apply(GENERIC_KEY);
     public static final ConfigBoolean PRINT_DEBUG = new ConfigBoolean("printingDebug", false).apply(GENERIC_KEY);
     public static final ConfigBoolean REPLACE_FLUIDS_SOURCE_BLOCKS = new ConfigBoolean("replaceFluidSourceBlocks", true).apply(GENERIC_KEY);
